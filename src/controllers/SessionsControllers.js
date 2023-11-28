7f54450f4e82f5c7271ad9805b7225986e1a5dd5
Lucas Felipe Lima Cid <lucasfelipaa@gmail.com>
2023-11-28 11:31:32 -0300

    changes in tests repos

--- src/controllers/SessionsControllers.js
+++ src/controllers/SessionsControllers.js
@@ -1,3 +1,4 @@
+const SessionsRepositoryTest = require('../repositories/sessions-test-prod/SessionsRepository-test')
 const SessionsRepository = require('../repositories/sessions-test-prod/SessionsRepository')
 const SessionsCreateSevice = require('../services/SessionsCreateService')
 const AppError = require('../utils/AppError')
@@ -7,7 +8,7 @@ class SessionsControllers{
     async create(request, response){
         const {email, password} = request.body
 
-        const sessionsRepository = new SessionsRepository()
+        const sessionsRepository = new SessionsRepositoryTest()
         const SessionsCreateService = new SessionsCreateSevice(sessionsRepository)
 
         const {user, token} = await SessionsCreateService.execute({email, password})
--- src/repositories/sessions-test-prod/SessionsRepository-test.js
+++ src/repositories/sessions-test-prod/SessionsRepository-test.js
@@ -1,15 +1,44 @@
+const AppError = require('../../utils/AppError')
+const {hash} = require('bcryptjs')
 
 class SessionsRepositoryTest{
-    testDatabase = []
+    testDatabase = [
+        {
+            name : "lucas",
+            password : "123",
+            email : "lucasfelipaa@gmail.com"
+        },
+        {
+            name : "andre",
+            password : "123",
+            email : "andre@gmail.com"
+        },
+        {
+            name : "maiana",
+            password : "123",
+            email : "maiana@gmail.com"
+        }
+    ] // the space that will have fictional data
     
-    async verifyCredentials(){
-
-    }
-
-    async insertInArray(){
-
+    async verifyCredentials({email, password}){
+       
+        const userInDatabase = this.testDatabase.filter(account =>  account.email == email && account.password == password)
         
+        if(userInDatabase.length == 0)throw new AppError('E-mail or password is invalid', 401)
+
+        const [user] = userInDatabase
+        
+        const objectJson = JSON.stringify(user)
+        
+        const token = await hash(objectJson, 8)
+        
+        return{
+            user,
+            token : token
+        }
     }
+
+  
 }
 
 module.exports = SessionsRepositoryTest
